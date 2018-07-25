8c600f7d01bb97e490bcf70e6d2720ed4094afc6
Kristen Truyol <kristentruyol@Kristens-Air.centurylink.net>
2018-07-25 19:19:57 -0400

    computer is logging correct, incorrect, and unanswered into console log properly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,6 +29,7 @@ $(document).ready(function () {
     function decrement() {
         gameTime--;
         console.log("timer going");
+    //function for user running out of time
 
         $("#timer").html("<h2>Time Remaining:  " + gameTime + "</h2>");
         if (gameTime < 1) {
@@ -36,30 +37,27 @@ $(document).ready(function () {
             clearInterval(timerGo);
             $("#timer").html("<h2>Time Remaining: " + gameTime + "</h2>");
             $("#quiz").hide();
-           
-    
+            gameResults();
         };
-        
-        
     }
 
     //function for user pressing done button prior to end of timer
     $("#done").on("click", function () {
         clearInterval(timerGo);
         $("#quiz").hide();
+        gameResults();
         console.log("user done");
     });
-    //function for user running out of time
    
-    //define correct answers for each question
-//array of objects for questions answeres
+    // for (let index = 0; index < questions.length; index++){
+    //     var questions = questions[index];
+    //     var answer = questions[index].correctAnswers;
+    
+//array of objects for questions and answers to be checked
 
-// for (let index = 0; index < questions.length; index ++){
-//     var question = questions[index];
-//     var answer = questions[index].correctAnswer
-// }
-$("#quiz").on("click",function(){
-    event.preventDefault();
+// $("#quiz").on("click",function(){
+//     event.preventDefault();
+function gameResults(){
     var answer1 = $("input[name=q-1]:checked").val();
     var answer2 = $("input[name=q-2]:checked").val();
     var answer3 = $("input[name=q-3]:checked").val();
@@ -70,12 +68,140 @@ $("#quiz").on("click",function(){
     var answer8 = $("input[name=q-8]:checked").val();
     var answer9 = $("input[name=q-9]:checked").val();
     var answer10 = $("input[name=q-10]:checked").val();
-    console.log(answer);
+    console.log("answer");
+   // results for question 1
     if("correct" === answer1){
-        correct++;
-        console.log();
+        correctAnswers++;
+        console.log("#1 is correct");
     }
+    else if("incorrect" === answer1){
+        incorrectAnswers++;
+        console.log("#1 is incorrect");
+    }
+    else if (undefined === answer1){
+        unAnswered++;
+        console.log("#1 is unanswered");
+    }
+    // results for question 2
+    if("correct" === answer2){
+        correctAnswers++;
+        console.log("#2 is correct");
+    }
+    else if("incorrect" === answer2){
+        incorrectAnswers++;
+        console.log("#2 is incorrect");
+    }
+    else if(undefined === answer2){
+        unAnswered++;
+        console.log("#2 is unanswered");
+    }
+    // results for question 3
+    if("correct" === answer3){
+        correctAnswers++;
+        console.log("#3 is correct");
+    }
+    else if("incorrect" === answer3){
+        incorrectAnswers++;
+        console.log("#3 is incorrect");
+    }
+    else if(undefined === answer3){
+        unAnswered++;
+        console.log("#3 is unanswered");
+    }
+     // results for question 4
+     if("correct" === answer4){
+        correctAnswers++;
+        console.log("#4 is correct");
+    }
+    else if("incorrect" === answer4){
+        incorrectAnswers++;
+        console.log("#4 is incorrect");
+    }
+    else if(undefined === answer4){
+        unAnswered++;
+        console.log("#4 is unanswered");
+    }
+     // results for question 5
+     if("correct" === answer5){
+        correctAnswers++;
+        console.log("#5 is correct");
+    }
+    else if("incorrect" === answer5){
+        incorrectAnswers++;
+        console.log("#5 is incorrect");
+    }
+    else if(undefined === answer5){
+        unAnswered++;
+        console.log("#5 is unanswered");
+    }
+     // results for question 6
+     if("correct" === answer6){
+        correctAnswers++;
+        console.log("#6 is correct");
+    }
+    else if("incorrect" === answer6){
+        incorrectAnswers++;
+        console.log("#6 is incorrect");
+    }
+    else if(undefined === answer6){
+        unAnswered++;
+        console.log("#6 is unanswered");
+    }
+     // results for question 7
+     if("correct" === answer7){
+        correctAnswers++;
+        console.log("#7 is correct");
+    }
+    else if("incorrect" === answer7){
+        incorrectAnswers++;
+        console.log("#7 is incorrect");
+    }
+    else if(undefined === answer7){
+        unAnswered++;
+        console.log("#7 is unanswered");
+    }
+     // results for question 8
+     if("correct" === answer8){
+        correctAnswers++;
+        console.log("#8 is correct");
+    }
+    else if("incorrect" === answer8){
+        incorrectAnswers++;
+        console.log("#8 is incorrect");
+    }
+    else if(undefined === answer8){
+        unAnswered++;
+        console.log("#8 is unanswered");
+    }
+     // results for question 9
+     if("correct" === answer9){
+        correctAnswers++;
+        console.log("#9 is correct");
+    }
+    else if("incorrect" === answer3){
+        incorrectAnswers++;
+        console.log("#9 is incorrect");
+    }
+    else if(undefined === answer3){
+        unAnswered++;
+        console.log("#9 is unanswered");
+    }
+     // results for question 10
+     if("correct" === answer10){
+        correctAnswers++;
+        console.log("#10 is correct");
+    }
+    else if("incorrect" === answer10){
+        incorrectAnswers++;
+        console.log("#10 is incorrect");
+    }
+    else if(undefined === answer10){
+        unAnswered++;
+        console.log("#10 is unanswered");
+    }
+}
 });
+
     
 
   
@@ -86,4 +212,3 @@ $("#quiz").on("click",function(){
 
     //display # unanswered
 
-})
