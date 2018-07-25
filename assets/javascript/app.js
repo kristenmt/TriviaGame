33adb05ff5650931918e744a6505d40cae3ac51f
Kristen Truyol <kristentruyol@Kristens-Air.centurylink.net>
2018-07-24 21:14:17 -0400

    hide function and show function working for quiz questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+
+//var for quiz timer
+var gameTime = 120;
+//var for # of correct questions
+var correctAnswers = 0;
+//var for # of incorrect questions
+var incorrectAnswers = 0;
+//var for # of unanswered questions
+var unAnswered = 0;
+//to hold gametime for running timer
+var timerGo;
+
+$(document).ready(function(){
+//hide quiz questions
+$("#quiz").hide();
+
+//click start button to begin the game and timer
+
+$("#start-game").on("click", function() {
+    
+    $("#quiz").show();
+    timerGo = setInterval(decrement, 1000);
+  });
+function decrement(){
+    gameTime--;
+}
+$("#timer").html(gameTime);
+
+
+//function for user pressing done button prior to end of timer
+
+//function for user running out of time
+
+//define correct answers for each question
+
+//if-else statements for checking each question and answer
+
+//display # correct
+
+//display # incorrect
+
+//display # unanswered
+
+})
